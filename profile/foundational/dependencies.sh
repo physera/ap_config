ca611068affc7430aa9535c2b596f24a5f0adfb6
Alex <alex@physera.com>
2020-05-07 11:31:39 -0500

    dependency checking

--- profile/foundational/dependencies.sh
+++ profile/foundational/dependencies.sh
@@ -3,15 +3,41 @@
 # jq
 
 
-echo "## Performing dependency check"
+# CHECK "autoenv" "brew list autoenv" "brew install autoenv"
+CHECK() {
+    local tool_name=$1
+    local check_cmd=$2
+    local install_cmd=$3
 
+    if [[ $(eval "${check_cmd} 2>&1") ]]; then
+        echo "## ${tool_name} is ready to go"
+    else
+        echo "## installing ${tool_name}"
+        eval $install_cmd
+    fi
+}
+
+# CHECK_MANUAL "docker" "command -v docker" "head to https://docs.docker.com/docker-for-mac/install/ and follow the instructions"
+CHECK_MANUAL() {
+    local tool_name=$1
+    local check_cmd=$2
+    local instructions=$3
+}
+
+
+# BASE DEPS
+
+echo "## Performing dependency check"
 command -v gimme-aws-creds >/dev/null 2>&1 || pip install --upgrade wheel gimme-aws-creds && asdf reshim python # TODO (not generic)
 command -v gimme-aws-creds >/dev/null 2>&1 || echo "Failed to install gimme-aws-creds, possible problem with pip"
 
 
-# from coda guide
+
+
+# from coda guide (these should be moved to scripts for eadch project)
 command -v virtualenv >/dev/null 2>&1 || pip install virtualenv
-brew list autoenv >/dev/null 2>&1 || brew install autoenv
-source "$(brew --prefix autoenv)/activate.sh"
+
+# for mobile app
+CHECK android-studio 'brew list android-studio' 'brew cask install android-studio'
 
 echo "## Dependency check passed"
